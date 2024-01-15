54949d3fbdbfdcbf2cbeb716d6dc0675be9bbbe1
garbich <131876643+garbich@users.noreply.github.com>
2024-01-15 13:39:24 +0200

    Update Hero.cs

--- Project-Game/Project-Game/Hero.cs
+++ Project-Game/Project-Game/Hero.cs
@@ -1,15 +1,15 @@
-﻿namespace Myspace
+namespace Myspace
 {
     internal class Hero
     {
         public string Name { get; set; }
         public int Health { get; set; }
-        public int AttackPower { get; set; }
+        public double AttackPower { get; set; }
         public int ResistanceToPhysical { get; set; }
         public int ResistanceToMagical { get; set; }
       
 
-        public Hero(string Name, int Health, int AttackPower, int ResistanceToPhysical, int ResistanceToMagical)
+        public Hero(string Name, int Health, double AttackPower, int ResistanceToPhysical, int ResistanceToMagical)
         {
             this.Name = Name;
             this.Health = Health;
